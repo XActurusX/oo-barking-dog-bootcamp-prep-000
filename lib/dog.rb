0467e97d00a58350b4626d14c76399cf25dd804f
John Jordan Shelley <jshelley553@gmail.com>
2019-06-11 05:47:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog 
+
+  def name=(names)
+    @name = names 
+  end 
+
+  def name 
+    @name 
+  end 
+
+  def bark
+    puts "Woof!"
+  end 
+end 
+
+fido.name("Fido")
+fido.bark
